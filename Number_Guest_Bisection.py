f8914ee0afa8762dcd4a89823e0d9dcd70b058f4
datcua <datcua9x@gmail.com>
2019-10-22 14:40:49 +0700

    finished Number_Guest_Game using Bisection_Search

--- Number_Guest_Bisection.py
+++ Number_Guest_Bisection.py
@@ -1,7 +1,8 @@
 low = 0
 high = 100
 guess = (low + high) / 2.0
-print("is your secrete number is", guess)
+print("Please think of a number between 0 and 100!")
+print("Is your secret number", guess, '?')
 x = input
 while x != 'c':
     x = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to "
@@ -9,12 +10,12 @@ while x != 'c':
     if x == 'h':
         high = guess
         guess = int((low + high) / 2)
-        print("is your secrete number is", guess)
-    elif x == 'l':
+        print("is your secrete number is", guess, '?')
+    if x == 'l':
         low = guess
         guess = int((low + high) / 2)
-        print("is your secrete number is", guess)
-    else:
+        print("is your secrete number is", guess, '?')
+    if x != 'l' and x!= 'h' and x!='c':
         print("Sorry, I did not understand your input")
-if x == 'c':
-    print("game over,your secrete number is ", guess)
+    if x == 'c':
+        print("game over,your secrete number is ", guess)
