9a2aab5821bf53c64f80f04da6eab250b4726dad
datcua <datcua9x@gmail.com>
2019-10-26 01:26:20 +0700

    finished calculate the minimum payment each month

---
+++ Paying_Debt_Excercise/Paying_Debt_Off_in_a_Year.py
@@ -0,0 +1,14 @@
+def Calculate(balance, annual_Interest_Rate):
+    monthly_Interest_Rate = annual_Interest_Rate / 12.
+    time = 12
+    for i in range(10, balance, 10):
+        b = balance
+        n = 0
+        while n < time:
+            n += 1
+            b = (b - i) * (1 + monthly_Interest_Rate)
+            if b <= 0:
+                return i
+
+
+print("the minimum payment each month is ",Calculate(3329, 0.2))
